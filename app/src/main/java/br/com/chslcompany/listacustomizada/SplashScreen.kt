1084bc10c6beb607f5f03fbb2cc21ea244556429
Carlos Henrique <chsl200293@gmail.com>
2018-03-18 16:33:26 -0300

    Lista Simples sem personalizacao

---
+++ app/src/main/java/br/com/chslcompany/listacustomizada/SplashScreen.kt
@@ -0,0 +1,33 @@
+package br.com.chslcompany.listacustomizada
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.chslcompany.listacustomizada.R
+import br.com.chslcompany.listacustomizada.MainActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    // Thread will sleep for 2 seconds
+                    Thread.sleep((2 * 1000).toLong())
+
+                    // After 2 seconds redirect to another intent
+                    startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+                    //Remove activity
+                    finish()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+
+            }
+        }
+        // start thread
+        background.start()
+    }
+}
